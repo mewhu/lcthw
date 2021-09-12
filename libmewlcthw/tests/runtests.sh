658c10fb90e4f19215e541441052fa266901b5bc
mew.hu <mew31716@gmail.com>
2021-09-12 10:12:32 +0800

    [Makefile] add \"_marked\" for avoid redundant testing already tests again

--- libmewlcthw/tests/runtests.sh
+++ libmewlcthw/tests/runtests.sh
@@ -1,7 +1,9 @@
 echo "Running unit tests:"
 
-for i in tests/*_tests
+#for i in tests/*_tests
+for j in ./tests/*_tests_marked
 do
+	i=$(echo $j | rev | cut -c 8- | rev)
 	if test -f $i
 	then
 		if $VALGRIND ./$i 2>> tests/tests.log
